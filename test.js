4beb28cbeb005c388d3ad63e3aa2451504f2d0b5
Franklin van de Meent <fr@nkl.in>
2016-03-18 03:55:33 +0100

    Test: added test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+var doTest = require ('.');
+
+doTest.add ('Module interface', function () {
+  doTest.test ()
+    .isObject ('fail', 'exports', doTest)
+    .isFunction ('fail', '.add', doTest.add)
+    .isFunction ('fail', '.run', doTest.run)
+    .isFunction ('fail', '.test', doTest.test)
+    .done ();
+});
+
+
+doTest.add ('Methods', function (fake) {
+  doTest.test ()
+    .isError ('fail', '.isError', new Error ())
+    .isObject ('fail', '.isObject', {})
+    .isArray ('fail', '.isArray', [])
+    .isString ('fail', '.isString', '')
+    .isNumber ('fail', '.isNumber', 1)
+    .isUndefined ('fail', '.isUndefined', fake)
+    .isBoolean ('fail', '.isBoolean', true)
+    .isFunction ('fail', '.isFunction', function () {})
+    .isExactly ('fail', '.isExactly', ':)', ':)')
+    .isNotEmpty ('fail', '.isNotEmpty', 'text')
+    .done ();
+});
+
+
+doTest.run ();
