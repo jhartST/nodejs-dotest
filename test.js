823dc84528f7d8e715305b99da711e9454f9a0e7
Franklin van de Meent <fr@nkl.in>
2016-05-05 07:38:36 +0200

    Test: added onExit() test

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 var doTest = require ('./');
 
+var testsDone = 0;
+
+
+// Tests
 doTest.add ('Module interface', function () {
   var test = doTest.test ();
 
@@ -9,6 +13,7 @@ doTest.add ('Module interface', function () {
     .isFunction ('fail', '.run', doTest.run)
     .isFunction ('fail', '.log', doTest.log)
     .isFunction ('fail', '.exit', doTest.exit)
+    .isFunction ('fail', '.onExit', doTest.onExit)
     .isFunction ('fail', '.test', doTest.test)
     .isFunction ('fail', '.test.done', test.done)
     .isFunction ('fail', '.test.good', test.good)
@@ -18,6 +23,8 @@ doTest.add ('Module interface', function () {
     .isFunction ('fail', '.test.info', test.info)
     .isFunction ('fail', '.test.exit', test.exit)
     .done ();
+
+  testsDone++;
 });
 
 
@@ -25,6 +32,8 @@ doTest.add ('test() shortcut', function (test) {
   doTest.test ()
     .isFunction ('fail', 'test', test)
     .done ();
+
+  testsDone++;
 });
 
 
@@ -52,6 +61,20 @@ doTest.add ('Methods', function (test, fake) {
     .good ('This is a good message')
     .info ({ hello: 'world' })
     .done ();
+
+  testsDone++;
+});
+
+
+doTest.add ('onExit', function (test) {
+  doTest.onExit (function (code) {
+    test ()
+      .info ('This is the onExit() callback')
+      .isNumber ('fail', 'code', code)
+      .isExactly ('warn', 'code', code, 0)
+      .isExactly ('warn', 'testsDone', testsDone, doTest.length)
+      .done ();
+  });
 });
 
 
