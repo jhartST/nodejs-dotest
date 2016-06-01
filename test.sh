257d37460ca51952963b1d27466f70333a32201f
Franklin van de Meent <fr@nkl.in>
2016-06-01 07:33:42 +0200

    test(fix): istanbul export should be report

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_REPO_SLUG" == "fvdm/nodejs-dotest" ]; then
-  istanbul cover test.js --print none --export lcovonly
+  istanbul cover test.js --print none --report lcovonly
   codeclimate-test-runner < ./coverage/lcov.info
 else
   node test.js
