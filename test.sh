01db72c7c0f1bba2f63909dc7c9d25595155d3df
Franklin van de Meent <fr@nkl.in>
2016-06-03 02:52:11 +0200

    test(script): Removed eslint duplicity

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 result=0
 
+eslint *.js || result=1
+
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$CODECLIMATE_REPO_TOKEN" != "" ]; then
-  eslint *.js || result=1
   istanbul cover test.js --print none --report lcovonly || result=1
   [ "$result" -eq "0" ] && codeclimate-test-reporter < ./coverage/lcov.info
 else
-  eslint *.js || result=1
   node test.js || result=1
 fi
 
