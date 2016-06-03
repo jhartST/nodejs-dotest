ea3060bf76108eaff528fc3691c19dc74646b822
Franklin van de Meent <fr@nkl.in>
2016-06-03 02:53:01 +0200

    test(script): Fail when lcov.info submit fails

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ eslint *.js || result=1
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$CODECLIMATE_REPO_TOKEN" != "" ]; then
   istanbul cover test.js --print none --report lcovonly || result=1
-  [ "$result" -eq "0" ] && codeclimate-test-reporter < ./coverage/lcov.info
+  [ "$result" -eq "0" ] && codeclimate-test-reporter < ./coverage/lcov.info || result=1
 else
   node test.js || result=1
 fi
