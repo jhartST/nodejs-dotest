7f1e3b68b45138b2f9a0042207d233f88ea2d192
Franklin van de Meent <fr@nkl.in>
2016-06-03 02:46:07 +0200

    test(script) Include eslint in master build

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 result=0
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$CODECLIMATE_REPO_TOKEN" != "" ]; then
+  eslint *.js || result=1
   istanbul cover test.js --print none --report lcovonly || result=1
   [ "$result" -eq "0" ] && codeclimate-test-reporter < ./coverage/lcov.info
 else
