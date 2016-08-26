ba45f2e8701ef351f43e49fa18f635265f1db5a1
Franklin van de Meent <fr@nkl.in>
2016-08-26 09:05:55 +0200

    Test(runner): More verbose console

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 result=0
 
-eslint *.js || result=1
+echo "Running ESLint..."
+./node_modules/.bin/eslint . || result=1
+echo
+
+./node_modules/.bin/istanbul cover test.js || result=1
 
 if [ "$TRAVIS" == "true" ]; then
-  istanbul cover test.js --print none --report lcovonly || result=1
-  [ "$result" -eq "0" ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || result=1
-else
-  node test.js || result=1
+  echo
+  echo "Sending coverage report to Coveralls..."
+  cat ./coverage/lcov.info | ./node_modules/.bin/coveralls || result=1
 fi
 
 exit $result
-
