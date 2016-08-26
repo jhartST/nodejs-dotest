1ae4f3ece72c17d0779366616d3ca3dbbbb94a31
Franklin van de Meent <fr@nkl.in>
2016-08-26 12:53:03 +0200

    Chore(runner): Added code comments

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+result=0
 nodebin=`pwd`/node_modules/.bin
+
+
+# Detect ancient npm version
 npmVersion=`npm -v`
 npmOld=`echo 'console.log ("$npmVersion" < "3");' | node`
-result=0
 
 if [ $npmOld == "true" ]; then
   nodebin=`pwd`/node_modules/dotest/node_modules/.bin
 fi
 
+
+# List commits since last release
 thisTag=`git describe --tags --abbrev=0`
 lastTag=`git describe --tags --abbrev=0 HEAD^`
 
@@ -21,12 +26,18 @@ git log $lastTag..HEAD --oneline --pretty=format:'%Cred%h%Creset -%C(yellow)%d%C
 echo
 echo
 
+
+# ESLint
 echo "Running ESLint..."
 $nodebin/eslint *.js lib/ test/ || result=1
 echo
 
+
+# Run test script with coverage
 $nodebin/istanbul cover test.js || result=1
 
+
+# Submit coverage to Coveralls.io
 if [ "$TRAVIS" == "true" ]; then
   echo
   echo "Sending coverage report to Coveralls..."
